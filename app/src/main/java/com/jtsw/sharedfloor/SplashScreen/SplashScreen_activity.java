f71aa6b7d6ce6b8c482678fe8fb609c52bc9bc1e
Julian <juliantrodom@gmail.com>
2016-11-07 16:53:11 +0100

    .

---
+++ app/src/main/java/com/jtsw/sharedfloor/SplashScreen/SplashScreen_activity.java
@@ -0,0 +1,42 @@
+package com.jtsw.sharedfloor.SplashScreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.jtsw.sharedfloor.R;
+import com.jtsw.sharedfloor.Selector_activity;
+
+public class SplashScreen_activity extends AppCompatActivity {
+
+    private static final int TIME = 4000;
+    private static final int DELAY=900;
+    private ImageView mImgvLogo,mImgName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_activity);
+        Animation animationImg= AnimationUtils.loadAnimation(SplashScreen_activity.this,R.anim.descend_animation);
+        Animation animationName= AnimationUtils.loadAnimation(SplashScreen_activity.this,R.anim.descend_animation);
+        animationName.setStartOffset(DELAY);
+
+        mImgvLogo=(ImageView)findViewById(R.id.splashIMVimg);
+        mImgName=(ImageView)findViewById(R.id.splashIMVname);
+
+        mImgvLogo.setAnimation(animationImg);
+        mImgName.setAnimation(animationName);
+
+        new Handler().postDelayed( new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen_activity.this, Selector_activity.class));
+                finish();
+            }
+        },TIME);
+    }
+}
